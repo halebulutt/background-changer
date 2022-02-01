d10961642e4ee1954d4565f813c44f37701c7b80
Hale Bulut <hale-972@hotmail.com>
2022-02-01 22:24:26 +0300

    file modified.

--- app.js
+++ app.js
@@ -19,13 +19,13 @@ button.addEventListener("click", changeBackground);
 let sequence = 0;
 
 function changeBackground() {
-    // rastgele bir renk.
+    // choose a random color.
     const randomNumber = Math.floor(Math.random() * colors.length);
     const chosenColor = colors[randomNumber];
     console.log(randomNumber, chosenColor);
     body.style.backgroundColor = chosenColor;
 
-    // sırasıyla arkaplan rengi seç. 
+    // choose background color respectively.
 
     // const chosenColor = colors[sequence];
     // if(sequence == 10) sequence = 0
