8842063df3876bcd0cd60fd1e16b469dabc1e6fd
Hale Bulut <hale-972@hotmail.com>
2022-02-03 01:50:55 +0300

    refactored code.

--- app.js
+++ app.js
@@ -1,38 +1,36 @@
 const body = document.querySelector("body");
-const button = document.querySelector("button");
+const changeButton = document.querySelector("#change-button");
+const sequenceButton = document.querySelector("#sequence-button");
+
 const colors = [
-"black", 
-"yellow",
-"#6dd6cc", 
-"blue",
-"cyan",
-"green",
-"#b25ecc",
-"#420a14", 
-"#1448d9",
-"#cc5e1f",
+    "black", 
+    "yellow",
+    "#6dd6cc", 
+    "blue",
+    "cyan",
+    "green",
+    "#b25ecc",
+    "#420a14", 
+    "#1448d9",
+    "#cc5e1f",
 ];
 
-
-button.addEventListener("click", changeBackground);
+changeButton.addEventListener("click", changeRandomBackground);
+sequenceButton.addEventListener("click", changeSequenceBackground);
 
 let sequence = 0;
 
-function changeBackground() {
+function changeRandomBackground() {
     // choose a random color.
     const randomNumber = Math.floor(Math.random() * colors.length);
     const chosenColor = colors[randomNumber];
-    console.log(randomNumber, chosenColor);
     body.style.backgroundColor = chosenColor;
+}
 
+function changeSequenceBackground() {
     // choose background color respectively.
-
-    // const chosenColor = colors[sequence];
-    // if(sequence == 10) sequence = 0
-    // sequence ++;
-
-    // body.style.backgroundColor = chosenColor;
-
-
-
+    const chosenColor = colors[sequence];
+    if (sequence == 10) sequence = 0
+    sequence ++;
+    body.style.backgroundColor = chosenColor;
 }
